25d067eb7cf5673d2b0865b3ab7f61c907cbbac9
Datian Zhang <DatianZhang@Datians-MacBook-Air.local>
2017-01-12 10:14:13 -0500

    Datian - player

--- Project/Player.java
+++ Project/Player.java
@@ -2,8 +2,40 @@
 // Bryan Chan, Eric Chen, Datian Zhang
 // APCS1 pd3
 
-public class Table {
+import cs1.Keyboard;
 
-    
-
+public class Player implements Comparable {
+    private String[] hand;
+    private float playerBet;
+    public Player(String firstCard, String secondCard, float bigBlind, float smallBlind){
+	hand = new String[2];
+	hand[0]=firstCard;
+	hand[1]=secondCard;
+	playerBet=bigBlind+smallBlind;
+    }
+    public String[] getHand(){
+	return hand;
+    }
+    public String toString(){
+	return hand[0] + " " + hand[1];
+    }
+    public float playerAction(float Amt){
+	if(Amt <= playerBet){
+	    System.out.println(
+			       "Welcome to Poker! I'm a bunny.\n " );
+	    //  option[0] + option[1] + option[2])	
+		}
+	return Keyboard.readFloat();
+    }
+    public int compareTo(Player a){
+	// object instanceof Type
+	System.out.println(a.getHand()[0]);	
+	return 0;
+    }
+    public static void main(String[] args){
+	Player Datian = new Player("AD", "AS",0,0);
+	Player Brien = new Player("5D", "3S",0,0);
+	System.out.println(Datian);
+	System.out.println(Brien.compareTo(Datian));
+    }
 }
