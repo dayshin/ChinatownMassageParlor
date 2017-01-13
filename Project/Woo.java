6f1b929fae8e94a9d73490cf2fa523b4d9780933
Bryan <bchan4@stuy.edu>
2017-01-13 00:23:53 -0500

    Basic game run

--- Project/Woo.java
+++ Project/Woo.java
@@ -21,17 +21,22 @@ public class Woo {
 	*/
 
 	Table game = new Table();
- 	System.out.println("Cards of the table");
-	System.out.println(game.getTC());
+	System.out.println("Cards of the table");
+	/* DIAG
+	System.out.println(game._deck.length);
+	for(int i = 0; i < game. _deck.length; i++){
+	    System.out.print(game._deck[i] + " ");
+	    }*/
+	/*System.out.println(game.getTC());
 	System.out.println();
 	System.out.println("Player1's cards: ");
 	System.out.println(game.getPC1());	
 	System.out.println("Player2's cards: ");
-	System.out.println(game.getPC2());	// END TESTING ZONE
-	
+	System.out.println(game.getPC2());      */
 
 
-
-    }
+	game.playGame();			   
+   
+    }//main
     
 }
